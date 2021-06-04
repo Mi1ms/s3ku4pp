5f07191bfd89214aa59c2e360103ce114c8cb8a3
Emilie G <emilie@neo-nomade.com>
2021-06-04 09:37:47 +0200

    init api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '71da694f2d5614317923c3a637d366aa'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '09acb387-f08b-4197-85a2-f88b33b8a4a0'
+};
